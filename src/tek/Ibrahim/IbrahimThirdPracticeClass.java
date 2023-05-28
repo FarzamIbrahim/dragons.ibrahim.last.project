c4d163652200d3a1a7a360ba747b27660def0a62
Ibrahim Farzam <ibra9419@gmail.com>
2023-05-28 13:16:06 -0400

    This is branch changing

--- src/tek/Ibrahim/IbrahimThirdPracticeClass.java
+++ src/tek/Ibrahim/IbrahimThirdPracticeClass.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("This is some branch changes");
 	System.out.println("branch changes");
 	System.out.println("Making some changes after branching");
+	System.out.println("New Branching");
 }
 }
