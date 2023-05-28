cbcd1ba0ff176c1277dcfea20a0dfad16aaa7322
Ibrahim Farzam <ibra9419@gmail.com>
2023-05-28 12:44:16 -0400

    This is last ubdate to my project

--- src/tek/Ibrahim/IbrahimThirdPracticeClass.java
+++ src/tek/Ibrahim/IbrahimThirdPracticeClass.java
@@ -3,5 +3,8 @@ package tek.Ibrahim;
 public class IbrahimThirdPracticeClass {
 public static void main(String[] args) {
 	System.out.println("This is self organized project");
+	System.out.println("This is some branch changes");
+	System.out.println("branch changes");
+	System.out.println("Making some changes after branching");
 }
 }
